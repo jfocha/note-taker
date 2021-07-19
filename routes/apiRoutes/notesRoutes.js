a8bfab1f5230ebb2e8773400d364b1dcfdb46412
jfocha <jfocha@gmail.com>
2021-07-18 23:55:24 -0700

    added delete

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,4 +1,4 @@
-const { findById, createNewNote } = require('../../lib/notes');
+const { findById, deleteNote, createNewNote } = require('../../lib/notes');
 const notes = require('../../db/db.json');
 const router = require('express').Router();
 
@@ -10,5 +10,23 @@ router.post('/notes', (req, res) => {
     const note = createNewNote(req.body, notes);
     res.json(note);
 });
+
+router.delete('/notes/:id', (req, res) => {
+    const userIndex = findById(req.params.id, notes)
+
+    if (userIndex === -1) return res.status(404).json({})
+   console.log(notes)
+
+   let index = 0;
+   for (let i = 0; i < notes.length; i++) {
+       if(notes[i] === userIndex){
+           index = i;
+       }
+   }
+
+    notes.splice(index, 1)
+deleteNote(notes)
+    res.json(notes)
+  })
 
 module.exports = router;
